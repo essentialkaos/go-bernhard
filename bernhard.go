1be9f3c7bcf05bd85e5ec74a609d83d148411d39
Anton Novojilov <andy@essentialkaos.com>
2022-03-29 02:22:26 +0300

    Migrate to go modules + Improve GitHub actions workflows

--- bernhard.go
+++ bernhard.go
@@ -2,7 +2,7 @@ package bernhard
 
 // ////////////////////////////////////////////////////////////////////////////////// //
 //                                                                                    //
-//                         Copyright (c) 2021 ESSENTIAL KAOS                          //
+//                         Copyright (c) 2022 ESSENTIAL KAOS                          //
 //      Apache License, Version 2.0 <https://www.apache.org/licenses/LICENSE-2.0>     //
 //                                                                                    //
 // ////////////////////////////////////////////////////////////////////////////////// //
@@ -14,7 +14,7 @@ import (
 	"regexp"
 	"sync"
 
-	"pkg.re/essentialkaos/ek.v12/req"
+	"github.com/essentialkaos/ek/v12/req"
 )
 
 // ////////////////////////////////////////////////////////////////////////////////// //
--- bernhard_test.go
+++ bernhard_test.go
@@ -2,7 +2,7 @@ package bernhard
 
 // ////////////////////////////////////////////////////////////////////////////////// //
 //                                                                                    //
-//                         Copyright (c) 2021 ESSENTIAL KAOS                          //
+//                         Copyright (c) 2022 ESSENTIAL KAOS                          //
 //      Apache License, Version 2.0 <https://www.apache.org/licenses/LICENSE-2.0>     //
 //                                                                                    //
 // ////////////////////////////////////////////////////////////////////////////////// //
@@ -12,7 +12,7 @@ import (
 	"testing"
 	"time"
 
-	. "pkg.re/essentialkaos/check.v1"
+	. "github.com/essentialkaos/check"
 )
 
 // ////////////////////////////////////////////////////////////////////////////////// //
